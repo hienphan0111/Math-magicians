161d21281ee1bf22ec330fa27ec34f13ab958d0c
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-02-13 20:12:49 +0700

    Fix linter err

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Fragment } from 'react';
-import { ReactDOM } from 'react-dom';
+import React from 'react';
 import logo from './image/logo-math.webp';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Math Magicians</h1>
-        <p>This is math app</p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Math Magicians</h1>
+      <p>This is math app</p>
+    </header>
+  </div>
+);
 
 export default App;
