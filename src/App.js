b62bf294efc138a780cefa3cd9efc14a0c106caf
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-02-13 18:04:24 +0700

    Fix linter err

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import logo from './image/logo-math.webp';
 import './App.css';
 
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Math Magicians</h1>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Math Magicians</h1>
+    </header>
+  </div>
+);
 
 export default App;
