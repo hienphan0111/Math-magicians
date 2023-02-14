44d764064147187da3e6b423e15d2cd83e8f8300
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-02-14 23:21:51 +0700

    Add calculator layout and style

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,26 +3,26 @@ import './Calculator.scss';
 
 const Calculator = () => (
   <div className="cal-container">
-    <span id="result">0</span>
-    <span id="ac">AC</span>
-    <span id="plus-minus">&#177;</span>
-    <span id="mod">%</span>
-    <span id="divide">&#247;</span>
-    <span id="multi">x</span>
-    <span id="minus">-</span>
-    <span id="plus">+</span>
-    <span id="equal">=</span>
-    <span id="9">9</span>
-    <span id="8">8</span>
-    <span id="7">7</span>
-    <span id="6">6</span>
-    <span id="5">5</span>
-    <span id="4">4</span>
-    <span id="3">3</span>
-    <span id="2">2</span>
-    <span id="1">1</span>
-    <span id="zero">0</span>
-    <span id="dot">.</span>
+    <span id="result" className="border-center">0</span>
+    <span id="ac" className="border-center">AC</span>
+    <span id="plus-minus" className="border-center">&#177;</span>
+    <span id="mod" className="border-center">%</span>
+    <span id="divide" className="orange">&#247;</span>
+    <span id="multi" className="orange">x</span>
+    <span id="minus" className="orange">-</span>
+    <span id="plus" className="orange">+</span>
+    <span id="equal" className="orange">=</span>
+    <span id="nine" className="border-center">9</span>
+    <span id="eight" className="border-center">8</span>
+    <span id="seven" className="border-center">7</span>
+    <span id="six" className="border-center">6</span>
+    <span id="five" className="border-center">5</span>
+    <span id="four" className="border-center">4</span>
+    <span id="three" className="border-center">3</span>
+    <span id="two" className="border-center">2</span>
+    <span id="one" className="border-center">1</span>
+    <span id="zero" className="border-center">0</span>
+    <span id="dot" className="border-center">.</span>
   </div>
 );
 
